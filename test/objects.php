82fafc755dd020d2c4335b51028c1c37d4b64a41
Daniel Frett <daniel.frett@ccci.org>
2012-02-29 21:38:32 +0000

    test xml generation of the key object

--- test/objects.php
+++ test/objects.php
@@ -1,13 +1,24 @@
 <?php
 
+require_once(dirname(__FILE__) . '/../source/Constants.php');
 require_once(dirname(__FILE__) . '/../source/Objects.php');
 
 class ObjectTest extends PHPUnit_Framework_TestCase {
 	public function testKey() {
+		# test generation of the key object
 		$keyVal = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopq';
 		$key = new GCXAuthz_Key('abcdefghijklmnopqrstuvwxyzabcdefghijklmnopq');
 		$this->assertTrue($key instanceof GCXAuthz_Key, "valid object");
 		$this->assertEquals($keyVal, $key->key(), 'key method returns correct value');
 		$this->assertEquals($keyVal, $key . '', 'key stringification works');
+
+		# test xml generation of key object
+		$expectedDom = new DOMDocument();
+		$expectedNode = $expectedDom->appendChild($expectedDom->createElementNS(XMLNS_GCXAUTHZ, 'key'));
+		$expectedNode->setAttribute('key', $keyVal);
+		$actualDom = new DOMDocument();
+		$actualNode = $actualDom->appendChild($key->toXml($actualDom));
+		$this->assertEquals($expectedDom->saveXml(), $actualDom->saveXml(), 'valid key xml');
+		$this->assertEqualXMLStructure($expectedNode, $actualNode, TRUE, 'valid key xml');
 	}
 }
