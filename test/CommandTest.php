eaeec43ea8c8099c3390d6cd31cd4dbbf64b122e
Daniel Frett <daniel.frett@ccci.org>
2012-03-02 18:50:59 +0000

    merge CommandXmlTest into CommandTest

--- test/CommandTest.php
+++ test/CommandTest.php
@@ -54,4 +54,36 @@ class CommandTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($ttl, $cmd->ttl());
 		$this->assertEquals(count($namespaces), count($cmd->namespaces()));
 	}
+
+	public function testGenerateLoginKeyXml() {
+		// simple command
+		$cmd = new \GCXAuthz\Command\GenerateLoginKey();
+		$expectedDom = new DOMDocument();
+		$expectedNode = $expectedDom->appendChild($expectedDom->createElementNS(\GCXAuthz\XMLNS, 'command'));
+		$expectedNode->setAttribute('type', 'generateLoginKey');
+		$actualDom = new DOMDocument();
+		$actualNode = $actualDom->appendChild($cmd->toXml($actualDom));
+		$this->assertEquals($expectedDom, $actualDom, 'valid simple generateLoginKey xml');
+
+		// command utilizing custom user, ttl, and multiple namespaces
+		$user = new \GCXAuthz\Object\User('GUEST');
+		$namespaces = array(
+			new \GCXAuthz\Object\Ns('ns1'),
+			new \GCXAuthz\Object\Ns('ns2')
+		);
+		$ttl = 5;
+		$cmd = new \GCXAuthz\Command\GenerateLoginKey($user, $namespaces, $ttl);
+		$expectedDom = new DOMDocument();
+		$expectedNode = $expectedDom->appendChild($expectedDom->createElementNS(\GCXAuthz\XMLNS, 'command'));
+		$expectedNode->setAttribute('type', 'generateLoginKey');
+		$expectedNode->setAttribute('ttl', $ttl);
+		$expectedNode->appendChild($user->toXml($expectedDom));
+		$nsNode = $expectedNode->appendChild($expectedDom->createElementNS(\GCXAuthz\XMLNS, 'namespaces'));
+		foreach($namespaces as $ns) {
+			$nsNode->appendChild($ns->toXml($expectedDom));
+		}
+		$actualDom = new DOMDocument();
+		$actualNode = $actualDom->appendChild($cmd->toXml($actualDom));
+		$this->assertEquals($expectedDom, $actualDom, 'valid full generateLoginKey xml');
+	}
 }
